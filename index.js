7bf64fae84ac2e1273e098c84f4357a72236b2be
kylehwelch <welch@udel.edu>
2022-08-06 21:26:35 -0400

    readme finished, messages not posting

--- index.js
+++ index.js
@@ -5,5 +5,62 @@ import MessageService from "./message-service.js";
 
     window.addEventListener("load", function () {
         document.getElementById("greeting").innerHTML = `Welcome, ${userId}!`;
-        messageService.getAllMessages();
+        messageService.getAllMessages()
+            .then(successCallback, errorCallback);
+
+        function successCallback(response) {
+            // This data comes from the resolves method
+            populateMessages(response);
+        }
+        function errorCallback(response) {
+            // This data comes from the reject method
+            console.log(response);
+        }
 });
+
+function populateMessages(messages) {
+    messages.forEach(message => {
+        addMessageToThread(message);
+    })
+}
+
+function createFormListener() {
+    const form = document.getElementById("new-message-form");
+
+    form.onsubmit = function (event) {
+        //stop by the regular form submission
+        event.preventDefault();
+
+        const data = {
+            fromid: userId,
+            message: form.message.value
+        };
+
+        messageService.createNewMessage(data)
+            .then(successCallback, errorCallback);
+
+        function successCallback(response) {
+            // This data comes from the resolve method
+            addMessageToThread(response);
+        }
+
+        function errorCallback(response) {
+            //This data comes back from the reject method
+            console.log(response);
+        }
+    }
+};
+
+function addMessageToThread(message) {
+    const messageListItem = document.createElement("LI");
+    const userIdHeading = document.createElement("h3");
+    const messageParagraph = document.createElement("p");
+    const messageContent = document.createTextNode(message.message);
+    const userIdContent = document.createTextNode(message.fromid);
+    userIdHeading.appendChild(userIdContent);
+    messageParagraph.appendChild(messageContent);
+    messageListItem
+        .appendChild(userIdHeading)
+        .appendChild(messageParagraph);
+    document.getElementById("message-list").appendChild(messageListItem);
+}
--- message-service.js
+++ message-service.js
@@ -1,22 +1,52 @@
 export default class MessageService {
 
     getAllMessages() {
-        let request = new XMLHttpRequest();
+        const request = new XMLHttpRequest();
 
-        new Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
         //Setup our listener to process completed requests
         request.onload = function() {
+            // Process the reponse
             if (request.status >= 200 && request.status < 300) {
-                const threads = (JSON.parse(request.responseText)); // 'This is the returned text.'
+                // If successful
+                const threads = (JSON.parse(request.responseText));
+                //this data is passed to the success callback
                 resolve(threads);
             } else {
-                console.log(`Error: ` + request.status); // An error occurred during the request.
+                // this data is passed on the success callback
+                reject({
+                    status: request.status,
+                    statusText: request.statusText
+                });
             }
         };
 
         request.open("GET", "http://zipcode.rocks:8085/messages");
 
         request.send();
+        })
+    }
+    createNewMessage(message) {
+        const request = new XMLHttpRequest();
+
+        return new Promise(function (resolve, reject) {
+            //Setup our listener to process completed requests
+            request.onload = function () {
+                // Process the response
+                if (request.status >= 200 && request.status < 300) {
+                    // If successful
+                    resolve(JSON.parse(request.responseText));
+                } else {
+                reject ({
+                    status: request.status,
+                    statusText: request.statusText
+                });
+            }
+        };
+
+        request.open("POST", `http://zipcode.rocks:8085/ids/${message.fromid}/messages`);
+
+        request.send(JSON.stringify(message));
         });
     }
 }
